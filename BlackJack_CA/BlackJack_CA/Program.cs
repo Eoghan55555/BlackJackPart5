1861f44e25312ce4caa2c103f437dacb53a06b5e
Eoghan55555 <123965051+Eoghan55555@users.noreply.github.com>
2023-11-01 11:20:26 +0000

    Add files via upload
    Tried to upload code by visualStudio and i realised it was not working as of now. 
    
    Moved the methods from the progam.cs into their respective classes and tested to see if they would work

---
+++ BlackJack_CA/BlackJack_CA/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace BlackJack_CA
+{
+    internal class Program
+    {
+        static List<string> CardSuits = new List<string>() { "Diamonds", "Hearts", "Clubs", "Spades" }; //List of Suits
+        static List<string> CardValues = new List<string>() { "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace" }; //List of Cards Types
+        static List<string> Deck = new List<string>(); //List of the actual Deck
+        static void Main(string[] args)
+        {
+            Card Player1 = new Card();//Tests for Card class
+            Player1.DeckReshuffle();
+            string card = Player1.GetCard();
+            Player1.GetCardValue(card);
+
+
+
+            Betting _Player1 = new Betting();//Tests for Betting class
+            _Player1.GetName();
+            _Player1.GetTotalBettingChips();
+            Console.WriteLine(_Player1.ToString());
+
+        }
+    }
+}
